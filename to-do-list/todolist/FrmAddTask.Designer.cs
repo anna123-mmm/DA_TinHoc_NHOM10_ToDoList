e2618ba56cab5b0c5e1ef5b0e909196fa6bb73be
hnaa.gn <tungletest1.email@gmail.com>
2024-12-17 12:10:53 +0700

    to-do-list

---
+++ to-do-list/todolist/FrmAddTask.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace todolist
+{
+    partial class FrmAddTask
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtStartDate = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.cbTag = new System.Windows.Forms.ComboBox();
+            this.btnDeleteTag = new System.Windows.Forms.Button();
+            this.btnAddTag = new System.Windows.Forms.Button();
+            this.txtRemind = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtTask
+            // 
+            this.txtTask.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTask.Location = new System.Drawing.Point(119, 71);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(320, 27);
+            this.txtTask.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Book Antiqua", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Task:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(156)))), ((int)(((byte)(193)))));
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(488, 33);
+            this.panel1.TabIndex = 4;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(194)))), ((int)(((byte)(210)))));
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(194)))), ((int)(((byte)(210)))));
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.ForeColor = System.Drawing.Color.Transparent;
+            this.btnExit.Image = global::todolist.Properties.Resources.exit;
+            this.btnExit.Location = new System.Drawing.Point(442, 2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(38, 29);
+            this.btnExit.TabIndex = 14;
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Book Antiqua", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Time to start:";
+            // 
+            // dtStartDate
+            // 
+            this.dtStartDate.CalendarMonthBackground = System.Drawing.Color.White;
+            this.dtStartDate.CalendarTitleBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.dtStartDate.CalendarTrailingForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.dtStartDate.CustomFormat = "MM/dd/yyyy  hh:mm tt";
+            this.dtStartDate.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtStartDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtStartDate.Location = new System.Drawing.Point(181, 122);
+            this.dtStartDate.Name = "dtStartDate";
+            this.dtStartDate.Size = new System.Drawing.Size(258, 25);
+            this.dtStartDate.TabIndex = 7;
+            this.dtStartDate.Value = new System.DateTime(2024, 12, 16, 0, 0, 0, 0);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Book Antiqua", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(49, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 22);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Tag:";
+            // 
+            // txtNote
+            // 
+            this.txtNote.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNote.Location = new System.Drawing.Point(47, 316);
+            this.txtNote.Multiline = true;
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(392, 95);
+            this.txtNote.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Book Antiqua", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(49, 281);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 22);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Note:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(112)))), ((int)(((byte)(146)))));
+            this.btnAdd.FlatAppearance.BorderSize = 0;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Bahnschrift Condensed", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(209, 442);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 33);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // cbTag
+            // 
+            this.cbTag.FormattingEnabled = true;
+            this.cbTag.Location = new System.Drawing.Point(119, 228);
+            this.cbTag.Name = "cbTag";
+            this.cbTag.Size = new System.Drawing.Size(245, 24);
+            this.cbTag.TabIndex = 14;
+            // 
+            // btnDeleteTag
+            // 
+            this.btnDeleteTag.BackColor = System.Drawing.Color.Transparent;
+            this.btnDeleteTag.BackgroundImage = global::todolist.Properties.Resources.delete__1_;
+            this.btnDeleteTag.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnDeleteTag.FlatAppearance.BorderSize = 0;
+            this.btnDeleteTag.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(156)))), ((int)(((byte)(193)))));
+            this.btnDeleteTag.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(156)))), ((int)(((byte)(193)))));
+            this.btnDeleteTag.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDeleteTag.Location = new System.Drawing.Point(411, 227);
+            this.btnDeleteTag.Name = "btnDeleteTag";
+            this.btnDeleteTag.Size = new System.Drawing.Size(28, 29);
+            this.btnDeleteTag.TabIndex = 16;
+            this.btnDeleteTag.UseVisualStyleBackColor = false;
+            this.btnDeleteTag.Click += new System.EventHandler(this.btnDeleteTag_Click);
+            // 
+            // btnAddTag
+            // 
+            this.btnAddTag.BackColor = System.Drawing.Color.Transparent;
+            this.btnAddTag.BackgroundImage = global::todolist.Properties.Resources.add_new;
+            this.btnAddTag.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnAddTag.FlatAppearance.BorderSize = 0;
+            this.btnAddTag.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(156)))), ((int)(((byte)(193)))));
+            this.btnAddTag.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(156)))), ((int)(((byte)(193)))));
+            this.btnAddTag.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddTag.Location = new System.Drawing.Point(373, 230);
+            this.btnAddTag.Name = "btnAddTag";
+            this.btnAddTag.Size = new System.Drawing.Size(29, 22);
+            this.btnAddTag.TabIndex = 15;
+            this.btnAddTag.UseVisualStyleBackColor = false;
+            this.btnAddTag.Click += new System.EventHandler(this.btnAddTag_Click);
+            // 
+            // txtRemind
+            // 
+            this.txtRemind.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRemind.Location = new System.Drawing.Point(209, 172);
+            this.txtRemind.Name = "txtRemind";
+            this.txtRemind.Size = new System.Drawing.Size(230, 27);
+            this.txtRemind.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Book Antiqua", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Remind before:";
+            // 
+            // FrmAddTask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(194)))), ((int)(((byte)(214)))));
+            this.ClientSize = new System.Drawing.Size(488, 508);
+            this.Controls.Add(this.btnDeleteTag);
+            this.Controls.Add(this.btnAddTag);
+            this.Controls.Add(this.cbTag);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNote);
+            this.Controls.Add(this.dtStartDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRemind);
+            this.Controls.Add(this.txtTask);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmAddTask";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmAddTask";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmAddTask_FormClosing);
+            this.Load += new System.EventHandler(this.FrmAddTask_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtStartDate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ComboBox cbTag;
+        private System.Windows.Forms.Button btnAddTag;
+        private System.Windows.Forms.Button btnDeleteTag;
+        private System.Windows.Forms.TextBox txtRemind;
+        private System.Windows.Forms.Label label3;
+    }
+}
